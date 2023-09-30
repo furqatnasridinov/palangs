7cf2d163db629a936d8dbcd705009047bfee96bf
nasridinov <nasridinrovfurkatjon@MacBook-Pro.bbrouter>
2023-09-30 16:07:06 +0500

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:palangs/app_widget.dart';
+import 'package:palangs/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const AppWidget());
 }
-
